f975a1e0a8772bb6a4e1c71bf63be8f3d0da7498
Miles Urquidi <milesurquidi@yahoo.com>
2025-04-30 19:08:23 -0700

    first push

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full">
+      <div className="text-4xl mt-10 font-bold flex justify-center">
+        MoodTube
+      </div>
+    </div>
+  );
+}
